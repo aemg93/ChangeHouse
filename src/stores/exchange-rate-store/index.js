94b164624a0be43fce2ae6105cba786c67f3c455
José R. Gutierrez <josefo727@gmail.com>
2024-12-19 15:01:23 -0500

    Remove unused imports of mutations in Pinia stores
    Unused `mutations` imports were removed from `exchange-rate-store` and `currency-store`. This cleanup reduces unnecessary code and improves maintainability. No functional changes were introduced.

--- src/stores/exchange-rate-store/index.js
+++ src/stores/exchange-rate-store/index.js
@@ -1,7 +1,6 @@
 import { defineStore } from 'pinia';
 import { state } from './state';
 import { getters } from './getters';
-import { mutations } from './mutations';
 import { actions } from './actions';
 
 export const useExchangeRateStore = defineStore({
