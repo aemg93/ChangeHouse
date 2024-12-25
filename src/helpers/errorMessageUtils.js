e53aff9e4152e48d8390d2bf9b37228ddae6a8fd
José R. Gutierrez <josefo727@gmail.com>
2024-12-25 16:49:30 -0500

    Add utility to format API error messages
    This utility formats error messages based on the provided and current dates. It ensures clarity by indicating whether data loaded is from today or a prior date.

---
+++ src/helpers/errorMessageUtils.js
@@ -0,0 +1,16 @@
+import { date } from 'quasar';
+
+const formatApiErrorMessage = (defaultDataDate) => {
+  const normalizedDate = defaultDataDate.replace(/-/g, '/');
+  const today = new Date();
+  const normalizedToday = date.formatDate(today, 'YYYY/MM/DD');
+
+  if (normalizedDate === normalizedToday) {
+    return 'No se pudieron obtener los datos. Se cargaron datos obtenidos hoy, en horas anteriores.';
+  } else {
+    const formattedDate = date.formatDate(normalizedDate, 'DD-MM-YYYY');
+    return `No se pudieron obtener los datos. Se cargaron datos obtenidos el ${formattedDate}.`;
+  }
+};
+
+export { formatApiErrorMessage };
