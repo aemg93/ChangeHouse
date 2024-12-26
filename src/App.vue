38e4bacec0ebb189593de5b46433260401854a6d
José R. Gutierrez <josefo727@gmail.com>
2024-12-26 11:08:59 -0500

    Remove Quasar notification logic and related styles.
    The mounted lifecycle method that displayed a Quasar notification was removed along with its associated styles and dependencies.

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@
 
 <script setup>
 import AppSpinnerOverlay from './components/AppSpinnerOverlay.vue';
-import { useQuasar } from 'quasar';
-const $q = useQuasar();
 
 
 defineOptions({
@@ -14,26 +12,11 @@ defineOptions({
   components: {
     AppSpinnerOverlay
   },
-  mounted() {
-    this.$q.notify({
-      message: 'Disfruta de nuestra app totalmente gratis y sin publicidad.',
-      color: 'positive',
-      position: 'top',
-      timeout: 3500,
-      icon: 'emoji_emotions',
-      classes: 'custom-notify-initial',
-    });
-  },
 });
 </script>
 
 <style>
 .q-page {
   min-height: calc(100vh - 300px) !important;
-}
-.custom-notify-initial {
-  font-size: 20px;
-  font-weight: 600;
-  text-align: center;
 }
 </style>
