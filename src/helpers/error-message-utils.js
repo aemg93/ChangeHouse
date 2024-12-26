e692d67e4253bd1292cd3a6b8034704dc8f08e95
José R. Gutierrez <josefo727@gmail.com>
2024-12-25 23:29:47 -0500

    Update error messages for improved clarity and consistency
    Adjusted the phrasing of error messages to start with "Error al obtener los datos" for better clarity.

--- src/helpers/error-message-utils.js
+++ src/helpers/error-message-utils.js
@@ -6,10 +6,10 @@ const formatApiErrorMessage = (defaultDataDate) => {
   const normalizedToday = date.formatDate(today, 'YYYY/MM/DD');
 
   if (normalizedDate === normalizedToday) {
-    return 'No se pudieron obtener los datos. Se cargaron datos obtenidos hoy, en consultas anteriores.';
+    return 'Error al obtener los datos: se cargaron datos obtenidos hoy, en consultas anteriores.';
   } else {
     const formattedDate = date.formatDate(normalizedDate, 'DD-MM-YYYY');
-    return `No se pudieron obtener los datos. Se cargaron datos obtenidos el ${formattedDate}.`;
+    return `Error al obtener los datos: se cargaron datos obtenidos el ${formattedDate}.`;
   }
 };
 
