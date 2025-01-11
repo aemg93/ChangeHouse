cecea6840f7e042d7431eb65e162a784538b3797
José R. Gutierrez <josefo727@gmail.com>
2025-01-10 22:57:19 -0500

    Add GoToIndexComponent with button linking to index page
    This component includes a button styled with Quasar that links to the homepage (index) using a router-link.

---
+++ src/components/GoToIndexComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-btn
+    clickable
+    tag="router-link"
+    :to="{ name: 'index' }"
+    color="blue"
+    class="q-px-sm"
+  >
+    <q-icon name="currency_exchange" />
+  </q-btn>
+</template>
+
+<script>
+  export default {};
+</script>
